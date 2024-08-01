3e6ff90616e9a0fbe4cc97470cc4188b6af3b1ec
nyadiia <nyadiia@pm.me>
2024-07-31 21:22:27 -0500

    feat: initial commit
    i'm thinking of porting over the entirity of neovim-flake over to here
    but for now it's sperated into my fork at
    https://github.com/nyadiia/neovim-flake

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  inputs.neovim-flake.url = "github:nyadiia/neovim-flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      neovim-flake,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        configModule.config = {
+          # Add any custom options (and feel free to upstream them!)
+          # options = ...
+
+          vim = {
+            lsp = {
+              enable = true;
+              formatOnSave = true;
+              lightbulb.enable = true;
+              nvimCodeActionMenu.enable = true;
+              trouble.enable = true;
+            };
+            languages = {
+              enableFormat = true;
+              enableTreesitter = true;
+              enableExtraDiagnostics = true;
+              enableLSP = true;
+              rust.enable = true;
+              html.enable = true;
+              markdown.enable = true;
+              nix = {
+                enable = true;
+                format.type = "nixfmt-rfc-style";
+              };
+            };
+            theme.enable = true;
+            theme.name = "gruvbox";
+            autocomplete.enable = true;
+            autopairs.enable = true;
+            filetree.nvimTreeLua.enable = true;
+            filetree.nvimTreeLua.resizeOnFileOpen = true;
+            git.enable = true;
+            git.gitsigns.enable = true;
+            keys.enable = true;
+            keys.whichKey.enable = true;
+            statusline.lualine.enable = true;
+            treesitter.enable = true;
+          };
+        };
+
+        nadiavim = neovim-flake.lib.neovimConfiguration {
+          modules = [ configModule ];
+          inherit pkgs;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell { nativeBuildInputs = [ nadiavim ]; };
+        packages.neovim = nadiavim;
+        apps.default = nadiavim;
+        overlay = final: prev: { inherit (self.packages) nadiavim; };
+      }
+    );
+}
